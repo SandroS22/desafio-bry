7c6ca570b7ad6dd07db18a2a867a6505449e142b
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2024-05-13 18:30:41 -0300

    initial commit

---
+++ backend-bry/src/main/java/com/sandros22/backendbry/BackendBryApplication.java
@@ -0,0 +1,13 @@
+package com.sandros22.backendbry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBryApplication.class, args);
+	}
+
+}
---
+++ backend-bry/src/test/java/com/sandros22/backendbry/BackendBryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandros22.backendbry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
