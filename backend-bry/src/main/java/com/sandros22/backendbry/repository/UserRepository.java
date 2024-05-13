112e8efe197ba34bbe3f8ff3b2cb6bc373c48b78
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2024-05-13 19:35:40 -0300

    feat: create service and repository

--- backend-bry/src/main/java/com/sandros22/backendbry/repository/UserRepository.java
+++ backend-bry/src/main/java/com/sandros22/backendbry/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.sandros22.backendbry.repository;
 
-public interface UserRepository {
+import com.sandros22.backendbry.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
--- backend-bry/src/main/java/com/sandros22/backendbry/service/UserService.java
+++ backend-bry/src/main/java/com/sandros22/backendbry/service/UserService.java
@@ -1,4 +1,30 @@
 package com.sandros22.backendbry.service;
 
+import com.sandros22.backendbry.entity.User;
+import com.sandros22.backendbry.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    public Optional<User> findByID(Integer userId) {
+            return userRepository.findById(userId);
+    }
+
+    public void deleteById(Integer userId) {
+        userRepository.deleteById(userId);
+    }
+
 }
