1686d35d1b034887a6ce455e44d460bfe21db63f
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2024-05-14 14:04:38 -0300

    s

--- backend-bry/src/main/java/com/sandros22/backendbry/service/UserService.java
+++ backend-bry/src/main/java/com/sandros22/backendbry/service/UserService.java
@@ -4,6 +4,7 @@ import com.sandros22.backendbry.entity.User;
 import com.sandros22.backendbry.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,14 +17,19 @@ public class UserService {
     }
 
     public void save(User user) {
+
         userRepository.save(user);
     }
 
-    public Optional<User> findByID(Integer userId) {
+    public Optional<User> findByID(String userId) {
             return userRepository.findById(userId);
     }
 
-    public void deleteById(Integer userId) {
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public void deleteById(String userId) {
         userRepository.deleteById(userId);
     }
 
