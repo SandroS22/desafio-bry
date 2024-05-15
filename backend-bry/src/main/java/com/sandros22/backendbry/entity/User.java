16f007eba3d5f528190f1ce2d2a1bb94a84b26b8
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2024-05-15 20:29:35 -0300

    refactor: add spring validation

--- backend-bry/src/main/java/com/sandros22/backendbry/entity/User.java
+++ backend-bry/src/main/java/com/sandros22/backendbry/entity/User.java
@@ -1,27 +1,39 @@
 package com.sandros22.backendbry.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import javax.imageio.stream.ImageInputStream;
 
 @Entity
 @Table(name = "users")
 public class User {
 
+
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column(nullable = false)
     private String cpf;
+    @Column(nullable = false)
     private String name;
     @Lob
-    private byte[] face;
+    private String face;
 
     public User() {
     }
 
-    public User(String cpf, String name, byte[] face) {
+    public User(String cpf, String name, String face) {
         this.cpf = cpf;
         this.name = name;
         this.face = face;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getCpf() {
@@ -40,11 +52,11 @@ public class User {
         this.name = name;
     }
 
-    public byte[] getFace() {
+    public String getFace() {
         return face;
     }
 
-    public void setFace(byte[] face) {
+    public void setFace(String face) {
         this.face = face;
     }
 }
