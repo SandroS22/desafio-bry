a8d35c158130e851baeec682d71dfb859f2b6317
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2024-05-15 20:30:23 -0300

    chore: remove spring security login necessecity

--- backend-bry/src/main/java/com/sandros22/backendbry/BackendBryApplication.java
+++ backend-bry/src/main/java/com/sandros22/backendbry/BackendBryApplication.java
@@ -2,8 +2,9 @@ package com.sandros22.backendbry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class BackendBryApplication {
 
 	public static void main(String[] args) {
