10ad05259fd6e452449a8a833851f370ed8dcbee
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2024-05-18 21:13:47 -0300

    feat: add create user

--- frontend-bry/src/app/services/users.service.ts
+++ frontend-bry/src/app/services/users.service.ts
@@ -1,23 +1,27 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user';
-import { HttpClientModule } from '@angular/common/http';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
-  private apiUrl: string = 'http://localhost:8080';
+  private apiUrl: string = 'http://localhost:8080/users';
   constructor(private httpClient: HttpClient) {}
 
 
+createUser(user: User): Observable<User>{
+  const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+  return this.httpClient.post<User>(`${ this.apiUrl }/create-user`, user, { headers });
+}
+
   getAll(): Observable<User[]>{
-    return this.httpClient.get<User[]>(`${ this.apiUrl }/users`);
+    return this.httpClient.get<User[]>(`${ this.apiUrl }`);
   }
 
   deleterUser(id: number): Observable<User>{
-    return this.httpClient.delete<User>(`${ this.apiUrl }/users/delete-user/${id}`);
+    return this.httpClient.delete<User>(`${ this.apiUrl }/delete-user/${id}`);
   }
 
 }
